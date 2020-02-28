9bab8886e53ae014d3bf6a4f5a57002efa005a7b
Meihdi Djebli <meihdi.djebli@cnam.fr>
2020-02-28 01:10:26 +0100

    Création de l'évaluation 1

---
+++ src/index.php
@@ -0,0 +1,80 @@
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Fête de la musique 2019</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
+    <div class="container">
+        <a class="navbar-brand" href="#"><i class="fa fa-music"></i> Fête de la musique 2019</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-home"></i> Accueil
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div id="lang">
+    <div class="d-flex justify-content-end">
+        <span class="mr-2">Changer de langue : </span>
+        <a class="mr-2" href="#">Anglais</a>
+        <a class="mr-2" href="#">Espagnol</a>
+        <a class="mr-2" href="#">Français</a>
+    </div>
+</div>
+
+<!-- Page Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h1 class="mt-5">Les évènements du 21 juin 2019</h1>
+        </div>
+    </div>
+
+    <div>
+        <table class="table table-hover table-primary">
+            <thead>
+            <tr>
+                <th>Evènement</th><th>Horaire</th><th>Lieu</th>
+            </tr>
+            </thead>
+            <tbody>
+            <!-- Exemple de présentation -->
+            <tr>
+                <td>2 be 3</td><td>21:00 à 00:00</td><td>Tourcoing</td>
+            </tr>
+            <!-- Fin de l'exemple -->
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+
+
+</body></html>
